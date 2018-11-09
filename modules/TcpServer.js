9de09b46d1d07aca96f1b0d4a225c6a60a2d27d0
laurent <htd.laurent@gmail.com>
2018-11-09 16:04:59 +0100

    remove localhost to listen

--- modules/TcpServer.js
+++ modules/TcpServer.js
@@ -1,19 +1,19 @@
 /*
  * Copyright 2018 SpinalCom - www.spinalcom.com
- * 
+ *
  * This file is part of SpinalCore.
- * 
+ *
  * Please read all of the following terms and conditions
  * of the Free Software license Agreement ("Agreement")
  * carefully.
- * 
+ *
  * This Agreement is a legally binding contract between
  * the Licensee (as defined below) and SpinalCom that
  * sets forth the terms and conditions that govern your
  * use of the Program. By installing and/or using the
  * Program, you agree to abide by all the terms and
  * conditions stated or referenced herein.
- * 
+ *
  * If you do not agree to abide by these terms and
  * conditions, do not demonstrate your acceptance and do
  * not install or use the Program.
@@ -36,7 +36,7 @@ class TcpServer {
     this.port = config.TCP_PORT_LISTEN;
     this.server = net.createServer();
 
-    this.server.listen(this.port, "localhost");
+    this.server.listen(this.port);
     _initEvent.call(this, onConnect, onData, onClose);
   }
 }
@@ -51,7 +51,7 @@ function _initEvent(onConnect, onData, onClose) {
       console.log("Address in use, retrying...");
       setTimeout(() => {
         this.server.close();
-        this.server.listen(this.port, "localhost");
+        this.server.listen(this.port);
       }, 1000);
     }
   });
